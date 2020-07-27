60cfe0b7dbda345b2ca81014b0e22a26cd230e33
name <43496101+Shihasz@users.noreply.github.com>
2020-07-27 12:22:54 +0530

    add first route

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+from models import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///stock.db"
@@ -13,3 +15,11 @@ ma = Marshmallow(app)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# return all companies
+@app.route('/companies', methods=['GET'])
+def companies():
+    companies_list = Company.query.all()
+    result = companies_schema.dump(companies_list)
+    return jsonify(result.data)
